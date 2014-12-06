2e02a3f6966ba835ff79199c2f2981a624dbdb19
newj <newj@users.noreply.github.com>
2014-12-06 16:18:34 +0100

    Update DemoClass.java
    Corrected some misspelled text

--- src/com/demoproject/demo/DemoClass.java
+++ src/com/demoproject/demo/DemoClass.java
@@ -3,7 +3,7 @@ package com.demoproject.demo;
 public class DemoClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello world!");
 	}
 
 }
